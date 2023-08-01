025bccb4f26f33b85bbd2c45042fffe199312c56
longhuux <huulong140103@gmail.com>
2023-08-01 18:03:24 +0700

    lesson5

--- b4/myreact/src/App.js
+++ b4/myreact/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ItemList from './components/lesson4/ItemList';
 import ItemBox from "./components/lesson4/ItemBox";
-import Modal from './components/lesson4/Modal.js';
+import SearchList from './components/lesson4/SearchList';
 
 function App() {
 
@@ -13,6 +13,19 @@ function App() {
     { id: 2, province: 'Ho Chi Minh', time: 'Tue 28 Nov 2021', description: 'Hanoi faith and hope. We always waiting for you!', image: 'https://lp-cms-production.imgix.net/2021-01/shutterstockRF_718619590.jpg' },
     { id: 3, province: 'Da Nang', time: 'Thu 28 Nov 2021', description: 'Hanoi faith and hope. We always waiting for you!', image: 'https://statics.vinpearl.com/cau-rong-da-nang_1657938152.jpg' },
   ]
+
+  // const books = [
+  //   { title: "The Alchemist", author: "Paulo Coelho", year: 1988, price: 10 },
+  //   { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960, price: 15 },
+  //   { title: "1984", author: "George Orwell", year: 1949, price: 20 },
+  //   { title: "Brave New World", author: "Aldous Huxley", year: 1932, price: 5 },
+  //   { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925, price: 25 },
+  // ];
+  // const bookTitlesAndPrices = books.map(book => ({ title: book.title, price: book.price }));
+
+
+  // const newArr = books.filter(book => { return book.price >5} )
+  // console.log(newArr);
 
   return (
     <div className="App">
@@ -31,7 +44,9 @@ function App() {
           ))}
         </div>
       </div>
+      <SearchList/>
     </div>
+    
   );
 }
 
